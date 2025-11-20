b33c96f450dfe8ec23772bbcc00d3e7218f07fb1
LulietLyan <lulietlyan@gmail.com>
2025-11-20 18:08:53 +0000

    Remove old script files after moving to scripts directory

--- Step-0-preparation.sh
+++
@@ -1,8 +0,0 @@
-clear
-pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple 
-pip install --upgrade pip
-pip3 install torch==2.1.2 torchvision torchaudio
-pip install sentencepiece
-pip install -r requirement.txt
-pip install -e .
-pip install nltk
